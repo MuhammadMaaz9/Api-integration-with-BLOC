fbefeef79bf175f697b55553aec3f5200c251939
Syed Musab <68772934+MusabBoltX@users.noreply.github.com>
2022-11-22 11:56:25 +0500

    Update services.dart

--- lib/sevices/services.dart
+++ lib/sevices/services.dart
@@ -2,7 +2,7 @@ import 'package:apibloc/models/models.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  Future getdata() async {
+  Future<Welcome?> getdata() async {
     var url = Uri.parse(
         'https://fluttertest.q3logics.com/api/getCities?device_id=aa1b0a9bab56fab2&token=a04c7e22085ecb0838fe4cc9b985b9a4&lang_id=1&country_id=189');
     var response = await http.get(url);
